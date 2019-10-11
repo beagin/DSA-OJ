4dcec979cda8b1b098c303a6ad45ee8b397e5fec
beagin <1600012607@pku.edu.cn>
2019-10-11 15:18:12 +0800

    edition on Little-Cutie

--- chapter02/04.cpp
+++ chapter02/04.cpp
@@ -1,14 +1,33 @@
-// by TYY
-// 2019.9.27
-
-// UNDONE
-
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
-int main(int argc, char const *argv[])
+bool function(vector<int> &seq)
 {
-    char x[201];
-    return 0;
+    int n = seq.size();             // 序列长度
+    int cur_index, pre_index = 0;   // 记录前一个索引与当前索引
+    // 创建一个range(1, n)的vector，记录剩余的编号
+    vector<int> temp;
+    for(int i = 0; i < n; ++i)
+        temp.push_back(i+1);
+
+    for (int i = 0; i < n - 1; ++i)
+    {
+        // 获取当前出栈的编号在temp中的索引
+        vector<int>::iterator iter = find(temp.begin(), temp.end(), seq[i]);
+        if (iter == temp.end())
+            return false;
+        cur_index = distance(temp.begin(), iter);
+
+        if (cur_index >= pre_index)
+        {
+            pre_index = cur_index - 1;
+            temp.erase(iter);   // 将当前编号从temp中删除
+        }
+        else
+            return false;
+    }
+    return true;
 }
