ffe4c81820f0459f0e46a4b0f63102a95f7ee599
Beagin <1600012607@pku.edu.cn>
2019-10-27 16:11:46 +0800

    version 20191027-2

--- chapter05/02.cpp
+++ chapter05/02.cpp
@@ -5,13 +5,35 @@
  */
 
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
+class HuffmanTreeNode {
+    int data;
+    HuffmanTreeNode *leftChild;
+    HuffmanTreeNode *rightChild;
+}
+
+class HuffmanTree {
+private:
+    HuffmanTreeNode *root;
+    
+}
+
+
 int main(int argc, char const *argv[])
 {
+    /// 接受输入
     int n;      // 外部结点的个数
     cin >> n;
+    int *nums = new int[n];
+    for (int i = 0; i < n; ++i) {
+        cin >> nums[i];
+    }
+    sort(nums, nums+n);     // 对数组进行排序
+
     
 
     return 0;
