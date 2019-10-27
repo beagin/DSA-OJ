7e59dc629985ae2cc755111d96f017a2b4ec8dc4
Beagin <1600012607@pku.edu.cn>
2019-10-27 12:52:53 +0800

    version 20191027-1

--- chapter05/01.cpp
+++ chapter05/01.cpp
@@ -5,11 +5,9 @@
  */
 
 #include <iostream>
-#include <vector>
-#include <string>
 using namespace std;
 const int MAXNUM = 10000;
-int postOrder[MAXNUM], inOrder[MAXNUM], preOrder[MAXNUM];
+int postOrder[MAXNUM], inOrder[MAXNUM];
 
 /* 根据中序和后序序列得到前序
  * sindex_in, eindex_in：中根序列的首尾坐标
@@ -19,14 +17,13 @@ void getPre(int sindex_in, int eindex_in, int sindex_post, int eindex_post) {
     /// 获取根节点的值并输出
     int root = postOrder[eindex_post];      
     cout << root << ' ';
-    /// 如果总长度为3则直接输出左右叶节点的值
-    if (eindex_in - sindex_in == 2) {
-        cout << postOrder[sindex_post] << ' ' << postOrder[eindex_post];
+    /// 如果总长度为1则直接返回
+    if (eindex_in == sindex_in) {
         return;
     }
     /// 在中根序列中找到根节点的位置
     int root_in=-1;
-    for (int i = sindex_in+1; i < eindex_in; ++i) {
+    for (int i = sindex_in; i <= eindex_in; ++i) {
         if (inOrder[i] == root)
             root_in = i;
     }
@@ -34,12 +31,16 @@ void getPre(int sindex_in, int eindex_in, int sindex_post, int eindex_post) {
         cout << "error" << endl;
         return;
     }
-    /// 获取后根序列中的左右子树
+    /// 获取后根序列中的左右子树长度
     int len_left = root_in - sindex_in;                 // 左子树长度
     int len_right = eindex_in - len_left - sindex_in;   // 右子树长度
     /// 进行递归
-    getPre(sindex_in, root_in-1, sindex_post, sindex_post+len_left-1);  // 左子树
-    getPre(root_in+1, eindex_in, sindex_post+len_left, eindex_post-1);  // 右子树
+    // 左子树
+    if (len_left > 0)
+        getPre(sindex_in, root_in-1, sindex_post, sindex_post+len_left-1);
+    // 右子树
+    if (len_right > 0)
+        getPre(root_in+1, eindex_in, sindex_post+len_left, eindex_post-1); 
     return;
 }
 
@@ -47,10 +48,11 @@ void getPre(int sindex_in, int eindex_in, int sindex_post, int eindex_post) {
 int main(int argc, char const *argv[])
 {
     /// 接受输入（中序与后序遍历）
-    int i=0;
+    int i = 0;
     while (cin >> inOrder[i++])
         if (cin.get() != ' ')
             break;
+    i = 0;
     while (cin >> postOrder[i++])
         if (cin.get() != ' ')
             break;
---
+++ chapter05/02.cpp
@@ -0,0 +1,18 @@
+/* Q5.2 Huffman编码
+ * by TYY
+ * created: 2019.10.27
+ * last edit: 2019.10.27
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int n;      // 外部结点的个数
+    cin >> n;
+    
+
+    return 0;
+}
